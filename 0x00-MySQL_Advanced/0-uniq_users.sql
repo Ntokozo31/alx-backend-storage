2d8e512d32a7551418410fbd088c1a1d138b1c03
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-19 19:07:25 +0000

    added my database

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,4 @@
--- Use the database
-USE user_managemant;
-
--- Create user table
+-- Create a new user table if it does'nt already exist
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,

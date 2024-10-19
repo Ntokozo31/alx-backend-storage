bae3e422d5be0108d9714b0ffa304879a9c6b78d
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-19 18:43:54 +0000

    Added the auto_increment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,7 @@ USE user_managemant;
 
 -- Create user table
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 	);

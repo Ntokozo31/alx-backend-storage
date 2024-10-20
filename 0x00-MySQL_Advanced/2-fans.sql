f76e0ed56127aad779ea5b123706cba31c6bf1df
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-20 08:29:07 +0000

    2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,9 @@
+-- Select the correct database
+USE holberton;
+
 -- Creating a script that ranks country origins of bands
 
 SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bans
+FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans DESC:
+ORDER BY nb_fans DESC;

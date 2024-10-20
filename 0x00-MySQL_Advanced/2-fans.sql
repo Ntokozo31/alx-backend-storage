b0ad4fb160b6c3488181e6a2fb48bd64e06211f7
Ntokozo31 <dubentokozo45@yahoo.com>
2024-10-20 12:36:35 +0000

    Populated nb_fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -3,6 +3,11 @@ ALTER TABLE metal_bands
 ADD COLUMN nb_fans INT DEFAULT 0;
 
 
+-- Populate nb_fans
+UPDATE metal_bands
+SET nb_fans = fans;
+
+
 -- Now select the origin and sum the nb_fans
 SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
